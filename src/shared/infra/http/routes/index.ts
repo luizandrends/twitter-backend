64468ea366a2828be61eea5b142197fbc7938d26
Luiz Andre <luizxd234@gmail.com>
2020-06-14 19:26:11 -0300

    chore: First commit

---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import UserRoutes from '@modules/users/infra/http/routes/user.routes'
+
+
+const routes = Router();
+
+routes.use('/users', UserRoutes)
+
+export default routes;
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import routes from '@shared/infra/http/routes'
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server erro',
+  });
+});
+
+app.listen(3333);
