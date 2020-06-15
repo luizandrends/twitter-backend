101f8425907f0d6954d70f2d7a8ef479246c2fdd
Luiz Andre <luizxd234@gmail.com>
2020-06-14 23:32:10 -0300

    chore: Imports on server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 import routes from '@shared/infra/http/routes';
 import AppError from '@shared/errors/AppError';
 
