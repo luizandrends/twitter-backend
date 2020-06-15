3296c47910371b91be7b1cd0e597381fe5d7987c
Luiz Andre <luizxd234@gmail.com>
2020-06-15 00:45:50 -0300

    feat: routes and controller

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserRoutes from '@modules/users/infra/http/routes/user.routes';
+import SessionRoutes from '@modules/users/infra/http/routes/session.routes';
 
 const routes = Router();
 
 routes.use('/users', UserRoutes);
+routes.use('/sessions', SessionRoutes);
 
 export default routes;
