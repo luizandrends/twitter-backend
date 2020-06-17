88f170774bda6b84ecbe346be2df22508d057b8c
Luiz Andre <luizxd234@gmail.com>
2020-06-17 18:42:44 -0300

    feature: creating fake saveFile and deleteFIle

---
+++ src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+
+import IStorageProvider from '../models/IStorageProvider';
+
+import File from '../typeorm/entities/File';
+
+class FakeStorageProvider implements IStorageProvider {
+  private storage: File[] = [];
+
+  public async saveFile(
+    originalname: string,
+    filename: string
+  ): Promise<File | undefined> {
+    const url = `http://localhost:3333/${originalname}`;
+
+    const file = new File();
+
+    Object.assign(file, { id: uuid() }, originalname, filename, url);
+
+    return file;
+  }
+
+  public async deleteFile(id: string): Promise<void> {
+    const findIndex = this.storage.findIndex(
+      storageFile => storageFile.id === id
+    );
+
+    this.storage.splice(findIndex, 1);
+  }
+}
+
+export default FakeStorageProvider;
--- src/shared/container/providers/StorageProvider/models/IStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/models/IStorageProvider.ts
@@ -1,6 +1,6 @@
 import File from '../typeorm/entities/File';
 
 export default interface IStorageProvider {
-  saveFile(originalname: string, filename: string): Promise<File>;
+  saveFile(originalname: string, filename: string): Promise<File | undefined>;
   deleteFile(id: string): Promise<void>;
 }
