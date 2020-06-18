da83092272ef5e2c8202d4755368a2b284b0e9d6
Luiz Andre <luizxd234@gmail.com>
2020-06-17 23:33:29 -0300

    feature: create users fields on files table

---
+++ src/shared/infra/typeorm/migrations/1592447257652-CreateUsersFieldOnFilesTable.ts
@@ -0,0 +1,39 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateUsersFieldOnFilesTable1592447257652
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'files',
+      new TableColumn({
+        name: 'user_id',
+        type: 'uuid',
+        isNullable: true,
+        default: null,
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'files',
+      new TableForeignKey({
+        name: 'FileUser',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'files',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('files', 'FileUser');
+
+    await queryRunner.dropColumn('files', 'user_id');
+  }
+}
