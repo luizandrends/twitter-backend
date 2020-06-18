16ab307ed015e66d636898563888a6459002b99d
Luiz Andre <luizxd234@gmail.com>
2020-06-17 22:20:56 -0300

    feature: it should not be able to update a avatar from an unexistent user

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,21 +1,57 @@
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
 import UpdateUserAvatarService from './UpdateUserAvatarService';
+import CreateUser from './CreateUserService';
 
 let fakeStorageProvider: FakeStorageProvider;
+let fakeHashProvider: FakeHashProvider;
+let fakeUsersRepository: FakeUsersRepository;
+
+let createUser: CreateUser;
 let updateAvatar: UpdateUserAvatarService;
 
-describe('CreateUser', () => {
+describe('UpdateAvatar', () => {
   beforeEach(() => {
     fakeStorageProvider = new FakeStorageProvider();
-    updateAvatar = new UpdateUserAvatarService(fakeStorageProvider);
+    fakeHashProvider = new FakeHashProvider();
+    fakeUsersRepository = new FakeUsersRepository();
+
+    createUser = new CreateUser(fakeUsersRepository, fakeHashProvider);
+
+    updateAvatar = new UpdateUserAvatarService(
+      fakeStorageProvider,
+      fakeUsersRepository
+    );
   });
 
   it('should be able to update the avatar', async () => {
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      username: '@johndoe',
+      password: '1234',
+    });
+
     const avatar = await updateAvatar.execute({
       originalname: 'originalname.jpg',
       filename: 'filename.jpg',
+      user_id: user.id,
     });
 
     expect(avatar?.name).toBe('originalname.jpg');
+  });
+
+  it('sould not be able to update a avatar from an unexistent user', async () => {
+    expect(
+      updateAvatar.execute({
+        originalname: 'originalname.jpg',
+        filename: 'filename.jpg',
+        user_id: 'non-existing-user',
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,25 +1,37 @@
 import { inject, injectable } from 'tsyringe';
 
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
-
 import File from '@shared/container/providers/infra/typeorm/entities/File';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
   originalname: string;
   filename: string;
+  user_id: string;
 }
 
 @injectable()
 class CreateUserService {
   constructor(
     @inject('StorageProvider')
-    private storageProvider: IStorageProvider
+    private storageProvider: IStorageProvider,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
   ) {}
 
   public async execute({
     originalname,
     filename,
+    user_id,
   }: IRequest): Promise<File | undefined> {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('You cannot update the avatar of an unexistent user');
+    }
+
     const file = await this.storageProvider.saveFile(originalname, filename);
 
     return file;
