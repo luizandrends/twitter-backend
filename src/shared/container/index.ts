a7d1910b487f1aa33313c07837c35c0b8976a164
Luiz Andre <luizxd234@gmail.com>
2020-06-23 19:25:04 -0300

    feat: UserTokensRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,9 @@ import './providers/index';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+// import IUserTokens from '@modules/users/repositories/IUserTokensRepository';
+// import UserTokensRepositories from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
