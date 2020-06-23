cdb62d0ad53875fe220aafc303fac2a21a8c6477
Luiz Andre <luizxd234@gmail.com>
2020-06-23 19:12:32 -0300

    feat: dependency injection of MailProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,12 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import StorageProvider from './StorageProvider/implementations/StorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+import MailProvider from './MailProvider/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   StorageProvider
 );
+
+container.registerInstance<IMailProvider>('MailProvider', new MailProvider());
