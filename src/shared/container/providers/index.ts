0c5f2eb04a16651800fb2d2302103f105e3d29d2
Luiz Andre <luizxd234@gmail.com>
2020-06-25 20:54:26 -0300

    chore: registering the singleton of MailTemplateProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,9 +6,17 @@ import StorageProvider from './StorageProvider/implementations/StorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import MailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   StorageProvider
 );
 
 container.registerInstance<IMailProvider>('MailProvider', new MailProvider());
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider
+);
