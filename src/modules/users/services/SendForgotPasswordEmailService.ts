14d0ed670a9db75f5123da696baee59031f9b0ca
Luiz Andre <luizxd234@gmail.com>
2020-06-25 21:45:20 -0300

    feat: creating template for password recovery

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -31,17 +32,24 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail({
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'templates',
+      'forgot_password.hbs'
+    );
+
+    await this.mailProvider.sendMail({
       to: {
         name: user.name,
         email: user.email,
       },
       subject: '[Twitter] Password recovery',
       templateData: {
-        template: 'Hello, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset?token=${token}`,
         },
       },
     });
