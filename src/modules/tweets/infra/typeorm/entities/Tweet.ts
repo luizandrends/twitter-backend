4d8e432a1e0b2c9dfbdd82cd14fef70c5778bf1a
Luiz Andre <luizxd234@gmail.com>
2020-06-29 19:05:01 -0300

    chore: model of tweets

---
+++ src/modules/tweets/infra/typeorm/entities/Tweet.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('tweets')
+class Tweets {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
+
+export default Tweets;
