cb803ce1ac450ea01082de178dc65cc83f707769
Luiz Andre <luizxd234@gmail.com>
2020-06-29 22:37:17 -0300

    chore: CreateTweetController

--- src/modules/tweets/infra/http/controllers/CreateTweetController.ts
+++ src/modules/tweets/infra/http/controllers/CreateTweetController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CreateTweetService from '@modules/tweets/services/CreateTweetService';
 
 class CreateTweetController {
-  public async create(
-    request: Request,
-    response: Response
-  ): Promise<Response> {}
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { content } = request.body;
+    const user_id = request.user.id;
+
+    const createTweet = container.resolve(CreateTweetService);
+
+    const tweet = await createTweet.execute({
+      content,
+      user_id,
+    });
+
+    return response.json(tweet);
+  }
 }
 
 export default CreateTweetController;
