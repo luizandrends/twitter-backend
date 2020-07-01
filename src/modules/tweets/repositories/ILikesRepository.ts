72eb1ba54dd366e4a621ef90186f248f99184612
Luiz Andre <luizxd234@gmail.com>
2020-07-01 20:55:40 -0300

    feat: adding methods of countLikes and listUsers

--- src/modules/tweets/repositories/ILikesRepository.ts
+++ src/modules/tweets/repositories/ILikesRepository.ts
@@ -1,6 +1,9 @@
+import User from '@modules/users/infra/typeorm/entities/User';
 import Like from '../infra/typeorm/entities/Like';
 
 export default interface ILikesRepository {
   create(tweet_id: string, user_id: string): Promise<Like>;
   delete(tweet_id: string): Promise<void>;
+  countLikes(tweet_id: string): Promise<number>;
+  listUsers(tweet_id: string): Promise<User[]>;
 }
--- src/modules/tweets/repositories/fakes/FakeLikesRepository.ts
+++ src/modules/tweets/repositories/fakes/FakeLikesRepository.ts
@@ -1,19 +1,40 @@
 import { uuid } from 'uuidv4';
 
+import User from '@modules/users/infra/typeorm/entities/User';
 import ILikesRepository from '../ILikesRepository';
 import Like from '../../infra/typeorm/entities/Like';
 
 class FakeLikesRepository implements ILikesRepository {
   private likes: Like[] = [];
 
+  private users: User[] = [];
+
   public async create(tweet_id: string, user_id: string): Promise<Like> {
     const like = new Like();
+    const user = new User();
 
     Object.assign(like, { id: uuid(), tweet_id, user_id });
 
     this.likes.push(like);
 
+    Object.assign(user, {
+      id: user_id,
+      name: 'John Doe',
+      username: '@johndoe',
+      password: '123',
+    });
+
+    this.users.push(user);
+
     return like;
+  }
+
+  public async countLikes(tweet_id: string): Promise<number> {
+    return this.likes.length;
+  }
+
+  public async listUsers(tweet_id: string): Promise<User[]> {
+    return this.users;
   }
 
   public async delete(like_id: string): Promise<void> {
