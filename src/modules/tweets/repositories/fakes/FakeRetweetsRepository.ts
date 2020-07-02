0f76a004eda29dfa9953ad31f5ca0e6485c0008f
Luiz Andre <luizxd234@gmail.com>
2020-07-02 20:15:00 -0300

    chore: fake retweet repository

---
+++ src/modules/tweets/repositories/fakes/FakeRetweetsRepository.ts
@@ -0,0 +1,69 @@
+import { uuid } from 'uuidv4';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import Retweet from '../../infra/typeorm/entities/Retweet';
+
+import IRetweetsRepository from '../IRetweetsRepository';
+
+class FakeRetweetsRepository implements IRetweetsRepository {
+  private retweets: Retweet[] = [];
+
+  private users: User[] = [];
+
+  public async create(tweet_id: string, user_id: string): Promise<Retweet> {
+    const retweet = new Retweet();
+    const user = new User();
+
+    Object.assign(retweet, { id: uuid(), tweet_id, user_id });
+
+    this.retweets.push(retweet);
+
+    Object.assign(user, {
+      id: user_id,
+      name: 'John Doe',
+      username: '@johndoe',
+      password: '123456',
+    });
+
+    this.users.push(user);
+
+    return retweet;
+  }
+
+  public async countRetweets(tweet_id: string): Promise<number> {
+    return this.retweets.length;
+  }
+
+  public async listUsers(tweet_id: string): Promise<Retweet[]> {
+    const retweets = this.retweets.filter(r => {
+      return r.tweet_id === tweet_id && r.user_id;
+    });
+
+    return retweets;
+  }
+
+  public async save(retweet: Retweet): Promise<void> {
+    const findIndex = this.retweets.findIndex(r => r.id === retweet.id);
+
+    this.retweets[findIndex] = retweet;
+  }
+
+  public async hasRetweet(
+    tweet_id: string,
+    user_id: string
+  ): Promise<Retweet | undefined> {
+    const findRetweet = this.retweets.find(
+      r => r.tweet_id === tweet_id && r.user_id === user_id
+    );
+
+    return findRetweet;
+  }
+
+  public async findRetweet(retweet_id: string): Promise<Retweet | undefined> {
+    const findRetweet = this.retweets.find(t => t.id === retweet_id);
+
+    return findRetweet;
+  }
+}
+
+export default FakeRetweetsRepository;
