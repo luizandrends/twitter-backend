8c9d0c393c9db709cb0be83bd7cf63e1538d3d31
Luiz Andre <luizxd234@gmail.com>
2020-07-02 00:27:55 -0300

    feat: count like controller and route

---
+++ src/modules/tweets/infra/http/controllers/CountLikeController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import CountLikeService from '@modules/tweets/services/CountLikeService';
+
+class CountLikeController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { tweet_id } = request.params;
+
+    const countLike = container.resolve(CountLikeService);
+
+    const like = await countLike.execute({
+      tweet_id,
+    });
+
+    return response.json(like);
+  }
+}
+
+export default CountLikeController;
--- src/modules/tweets/infra/http/routes/like.routes.ts
+++ src/modules/tweets/infra/http/routes/like.routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import CreateLikeController from '../controllers/CreateLikeController';
+import CountLikeController from '../controllers/CountLikeController';
 
 const createLikeController = new CreateLikeController();
+const countLikeController = new CountLikeController();
 
 const likeRouter = Router();
 
@@ -11,6 +13,12 @@ likeRouter.post(
   '/create/:tweet_id',
   ensureAuthenticated,
   createLikeController.create
+);
+
+likeRouter.get(
+  '/count/:tweet_id',
+  ensureAuthenticated,
+  countLikeController.create
 );
 
 export default likeRouter;
